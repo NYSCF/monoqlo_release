91e81b06f3abe2b3d5821c5073bba41462ae7021
brodie-brodes <58966874+brodie-brodes@users.noreply.github.com>
2021-12-07 09:59:03 -0500

    Update mq_models.py

--- mq_models.py
+++ mq_models.py
@@ -1,9 +1,9 @@
 from keras import models
 from keras_retinanet import models as krmodels
 
-def load_models(global_model_path='Models/multi_class_v1-1_epoch12.h5',
-                local_model_path='Models/interm_v1-1.h5',
-                sc_model_path='Models/sc_augmented_v0.0.h5', # Single cell model
+def load_models(global_model_path='Models/multi_class_v1-1_epoch52.h5',
+                local_model_path='Models/interm_v1-8.h5',
+                sc_model_path='Models/sc_augmented_v1.7.h5', # Single cell model
                 class_model_path = 'Models/2019-07-22onTG3_chkpt_model.22-acc0.94_V1.hdf5'):
 
     global_model = krmodels.load_model(global_model_path, backbone_name='resnet50')
